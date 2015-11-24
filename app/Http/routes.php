94be28ead0175fa5ad109803db3db461c9ad6073
emtudo <emtudo@gmail.com>
2015-11-24 19:15:37 -0200

    fixed routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,25 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/atualizar', function (\Church\Address $address) {
-    $addresses = $address->all();
-    foreach ($addresses as $address) {
-        $maps = new \Library\GMaps();
-        $add = $address->street.', '.$address->number.' - '.$address->district.', '.$address->city.' - '.$address->state;
-        if (isset($address->zipcode)) {
-            $zipcode = $address->zipcode;
-            $zipcode = preg_replace('/\D/', '', $zipcode);
-            $add .= ', '.substr($zipcode, 0, 5).'-'.substr($zipcode, 5, 3);
-        }
-        $add .= ', '.$address->country;
-
-        $location = $maps->location($add);
-
-        if (isset($location->lat) && isset($location->lng)) {
-            $address->latitude = $location->lat;
-            $address->longitude = $location->lng;
-        }
-        $address->save();
-    }
-});
